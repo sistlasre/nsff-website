ad29b6e8e45aeab85b529022aa3e96a103bc26bb
Sreeharsha Sistla <Sreeharsha@sree.lan>
2022-04-15 11:09:14 -0700

    Removing unnecessary file

--- templates/pages/home_page/urls.py
+++
@@ -1,8 +0,0 @@
-# helloworld/urls.py
-from django.conf.urls import url
-from django.conf.urls.static import static
-from .views import HomePageView
-
-urlpatterns = [
-    url(r'^$', HomePageView.as_view()),
-]
